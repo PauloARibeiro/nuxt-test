7b45e8e804f5afd9410c6d74f224e12290c501c0
Paulo <pauloribeirowork1@gmail.com>
2023-01-29 20:10:28 +0000

    test

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script lang="ts" setup>
+const { data: api } = await useFetch("/api/clients");
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <h1 class="text-persian-blue-500">
+      {{ api }}
+      <span class="text-gray-100" />
+    </h1>
   </div>
 </template>
